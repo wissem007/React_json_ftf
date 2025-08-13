31627c7db7c4696ded475fb97a82d04a0b584e17
aloui.wissem <alouiwiss@gmail.com>
2025-08-13 17:07:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -540,13 +540,13 @@ const App = () => {
                                     <div className={`p-3 text-white ${player.typeInterv === 'Joueur' ? 'bg-gradient-to-r from-green-500 to-green-600' : 'bg-gradient-to-r from-orange-500 to-orange-600'}`}>
                                       <div className="flex items-center justify-between">
                                         <div className="flex items-center space-x-2">
-                                          <div className="w-8 h-8 bg-white bg-opacity-20 rounded-full flex items-center justify-center">
+                                          <div className="w-10 h-10 bg-white bg-opacity-20 rounded-full flex items-center justify-center">
                                             {player.numLicence === "910919010" ? (
-                                              <img src="/images/910919010.jpeg" alt={`${player.name} ${player.lastName}`} className="w-6 h-6 rounded-full" />
+                                              <img src="/images/910919010.jpeg" alt={`${player.name} ${player.lastName}`} className="w-10 h-10 rounded-full" />
                                             ) : player.photo ? (
-                                              <img src={player.photo} alt={`${player.name} ${player.lastName}`} className="w-6 h-6 rounded-full" />
+                                              <img src={player.photo} alt={`${player.name} ${player.lastName}`} className="w-10 h-10 rounded-full" />
                                             ) : (
-                                              <Users size={16} />
+                                              <Users size={20} />
                                             )}
                                           </div>
                                           <div>
